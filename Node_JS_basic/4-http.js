1c78430534554ba67193d77195b448b4e1df1358
JBMjese <6935@holbertonstudents.com>
2024-06-06 16:39:10 -0500

    :boom: Point 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+// create a small HTTP server
+// which should listen on port 1245
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+module.exports = app;
