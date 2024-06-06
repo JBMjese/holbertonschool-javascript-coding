01b93208e4d6941720027eb691ba1c69ce757908
JBMjese <6935@holbertonstudents.com>
2024-06-06 16:43:38 -0500

    :zap: Fix Point 3

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -2,33 +2,37 @@
 // If the database is not available, it should throw a message error.
 // It should log the number of students in each field, and the list.
 
-const fs = require('fs');
+const fs = require("fs");
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
+    fs.readFile(path, "utf-8", (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
+        reject(new Error("Cannot load the database"));
         return;
       }
 
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const lines = data.split("\n").filter((line) => line.trim() !== "");
       const [, ...students] = lines;
       const fieldCounts = {};
 
       students.forEach((student) => {
-        const [, , , field] = student.split(',');
+        const [, , , field] = student.split(",");
         if (fieldCounts[field]) {
-          fieldCounts[field].push(student.split(',')[0]);
+          fieldCounts[field].push(student.split(",")[0]);
         } else {
-          fieldCounts[field] = [student.split(',')[0]];
+          fieldCounts[field] = [student.split(",")[0]];
         }
       });
 
       console.log(`Number of students: ${students.length}`);
       for (const field in fieldCounts) {
         if (fieldCounts) {
-          console.log(`Number of students in ${field}: ${fieldCounts[field].length}. List: ${fieldCounts[field].join(', ')}`);
+          console.log(
+            `Number of students in ${field}: ${
+              fieldCounts[field].length
+            }. List: ${fieldCounts[field].join(", ")}`
+          );
         }
       }
       resolve();
