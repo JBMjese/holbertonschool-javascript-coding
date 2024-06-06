26c0265e6371af0c6f65bd588c50be4990098567
JBMjese <6935@holbertonstudents.com>
2024-06-06 15:15:07 -0500

    :sparkles: Point 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,20 @@
+// It should show a login message
+// the user must be able to enter their name and then see it on the screen
+
+function StdinName() {
+  process.stdin.setEncoding('utf8');
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+  process.stdin.once('readable', () => {
+    const userInput = process.stdin.read();
+    if (userInput !== null && userInput !== '\n') {
+      process.stdout.write(`Your name is: ${userInput}`);
+    }
+    process.stdin.emit('end');
+  });
+
+  process.stdin.once('end', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
+StdinName();
