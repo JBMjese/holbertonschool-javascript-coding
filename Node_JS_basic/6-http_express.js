4176b1663f6c5fcbad595cfa99273d1db5347c4e
JBMjese <6935@holbertonstudents.com>
2024-06-07 06:59:46 -0500

    :dizzy: Fix Point 6

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -5,8 +5,10 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello Holberton School!");
 });
+
 const PORT = 1245;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
 module.exports = app;
