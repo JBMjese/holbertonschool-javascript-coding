f5439d7bf81bd023e163c96ff9890c7bd820c151
JBMjese <6935@holbertonstudents.com>
2024-06-07 07:29:49 -0500

    :boom: Point 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,42 +1,42 @@
-const express = require("express");
-const fs = require("fs").promises;
+const express = require('express');
+const fs = require('fs').promises;
 
 async function countStudents(path) {
   try {
-    const data = await fs.readFile(path, "utf8");
-    const rows = data.split("\n").filter((row) => row.trim());
-    const headers = rows.shift().split(",");
-    const fieldIndex = headers.indexOf("field");
-    const firstNameIndex = headers.indexOf("firstname");
-    const fields = [...new Set(rows.map((row) => row.split(",")[fieldIndex]))];
+    const data = await fs.readFile(path, 'utf8');
+    const rows = data.split('\n').filter((row) => row.trim());
+    const headers = rows.shift().split(',');
+    const fieldIndex = headers.indexOf('field');
+    const firstNameIndex = headers.indexOf('firstname');
+    const fields = [...new Set(rows.map((row) => row.split(',')[fieldIndex]))];
 
     let result = `Number of students: ${rows.length}\n`;
 
     fields.forEach((field) => {
       const students = rows.filter(
-        (row) => row.split(",")[fieldIndex] === field
+        (row) => row.split(',')[fieldIndex] === field
       );
       result += `Number of students in ${field}: ${
         students.length
       }. List: ${students
-        .map((student) => student.split(",")[firstNameIndex])
-        .join(", ")}\n`;
+        .map((student) => student.split(',')[firstNameIndex])
+        .join(', ')}\n`;
     });
 
     return result;
   } catch (error) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
 }
 
 const app = express();
 const port = 1245;
 
-app.get("/", (req, res) => {
-  res.send("Hello Holberton School!");
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.get("/students", async (req, res) => {
+app.get('/students', async (req, res) => {
   try {
     const data = await countStudents(process.argv[2]);
     res.send(`This is the list of our students\n${data}`);
