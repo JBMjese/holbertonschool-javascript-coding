f93b839e6cea41ca65555af6e019a3fa18737f10
JBMjese <6935@holbertonstudents.com>
2024-06-07 07:07:17 -0500

    :dizzy: Fix

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,23 +2,23 @@
 // If the database is not available, it should return an error message.
 // You must record the number of students in each field and list.
 
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, "utf8");
+    const data = fs.readFileSync(path, 'utf8');
 
-    const lines = data.split("\n").filter((line) => line.trim() !== "");
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
     const [, ...students] = lines;
     const fieldCounts = {};
 
     students.forEach((student) => {
-      const [, , , field] = student.split(",");
+      const [, , , field] = student.split(',');
 
       if (fieldCounts[field]) {
-        fieldCounts[field].push(student.split(",")[0]);
+        fieldCounts[field].push(student.split(',')[0]);
       } else {
-        fieldCounts[field] = [student.split(",")[0]];
+        fieldCounts[field] = [student.split(',')[0]];
       }
     });
 
@@ -28,12 +28,12 @@ function countStudents(path) {
         console.log(
           `Number of students in ${field}: ${
             fieldCounts[field].length
-          }. List: ${fieldCounts[field].join(", ")}`
+          }. List: ${fieldCounts[field].join(',')}`
         );
       }
     }
   } catch (error) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
 }
 module.exports = countStudents;
